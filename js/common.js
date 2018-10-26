09a10395c0bd3b6d66cc79ac7ccfe8cab3c44203
nohc <java.util.x@gmail.com>
2018-10-26 18:55:09 +0800

    first commit

---
+++ js/common.js
@@ -0,0 +1,45 @@
+
+$(function () {
+    let $s = $('.masonry');
+    if ($s.length > 0) {
+        $s.masonry({
+            // options...
+            itemSelector: '.masonry-item',
+            columnWidth: '.masonry-sizer',
+            percentPosition: true,
+        });
+    }
+});
+
+
+$('#sidebar-toggle').click(function (e) {
+    $('.app').toggleClass('is-collapsed');
+    e.preventDefault();
+
+    setTimeout(function () {
+        window.dispatchEvent(window.EVENT);
+    }, 300);
+});
+
+
+$(".sidebar .sidebar-menu li a").click(function (e) {
+    const $this = $(this);
+
+    if ($this.parent().hasClass('open')) {
+        $this.parent().children('.dropdown-menu').slideUp(200, function () {
+            $this.parent().removeClass('open');
+        });
+    } else {
+        $this.parent().parent().children('li.open').children('.dropdown-menu').slideUp(200);
+
+        $this.parent().parent().children('li.open').children('a').removeClass('open');
+
+        $this.parent().parent().children('li.open').removeClass('open');
+
+        $this.parent().children('.dropdown-menu').slideDown(200, function () {
+            $this.parent().addClass('open');
+        });
+    }
+});
+
+
