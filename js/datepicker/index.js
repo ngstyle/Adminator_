d734ac7e71c8a3d8eb8e493dd60deb4caa5c51cd
nohc <java.util.x@gmail.com>
2018-10-28 02:45:41 +0800

    datepicker changeDate callback

--- js/datepicker/index.js
+++ js/datepicker/index.js
@@ -1,4 +1,11 @@
 $(function () {
-    $('.start-date').datepicker();
-    $('.end-date').datepicker();
+    // https://bootstrap-datepicker.readthedocs.io/en/stable/events.html
+
+    $('.start-date').datepicker().on('changeDate',function () {
+        $('.start-date').datepicker('hide');
+    });
+
+    $('.end-date').datepicker().on('changeDate',function () {
+        $('.end-date').datepicker('hide');
+    });
 });
